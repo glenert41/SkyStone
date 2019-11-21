33df3c262c3e4f7c4a68cd8c5d72c11c1ac7c5a3
apaquet37 <apaquet37@charlottesvilleschools.org>
2019-11-21 12:32:29 -0500

    11/21 - Brief Autonomous added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BACONbotMechanum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BACONbotMechanum.java
@@ -139,7 +139,7 @@ public class BACONbotMechanum extends LinearOpMode {
 
 
 
-            //BUILD SIDE AUTO - IN PROGRESS (Tuning)----------
+            //BUILD SIDE AUTO - IN PROGRESS (Tuning)---------------------------------
             if (gamepad1.a)  {
                 while (robot.backDistance.getDistance(DistanceUnit.MM) < 790) {
                     driveBackwards();
@@ -160,7 +160,7 @@ public class BACONbotMechanum extends LinearOpMode {
                 //strafe left
                 strafeLeft();
             }
-            //----------------------
+            //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
             //Mat Servo Tuning
             double open_Pos = 0;
